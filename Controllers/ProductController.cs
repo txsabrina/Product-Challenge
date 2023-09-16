d690c5ebeb230588c9ebed87c3e5e0d75b90118a
Sabrina Teixeira <stx.sabrina@gmail.com>
2023-09-16 20:47:54 -0300

    feat: create controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq;
+
+public class ProductController
+{
+    private readonly Connection _context;
+
+    public ProductController(Connection context)
+    {
+        _context = context;
+    }
+
+
+    public void addProduct(Product product)
+    {
+        _context.Products.Add(product);
+        _context.SaveChanges();
+    }
+
+    public Product getById(int id)
+    {
+        return _context.Products.Find(id);
+        
+    }
+
+    public IQueryable<Product> getAll()
+    {
+        return _context.Product;
+    }
+
+    public void Update(int id, string name)
+    {
+        var productToUpdate = _context.Products.Find(id);
+        productToUpdate.Name = name;
+        _context.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+        var foundProduct = _context.Products.FindOrDefault(product => product.Id == id);
+        if(foundProduct != null) 
+        {
+            _context.Products.Remove(foundProduct);
+            _context.SaveChanges();
+        }
+    }
+}
